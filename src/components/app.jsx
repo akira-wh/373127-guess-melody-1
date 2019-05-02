b6190a33c0629ad1b136eea1943582dc68d57c30
Akira <154.4k1r4@gmail.com>
2019-05-02 20:37:38 +0600

    NEW: +Компонент App

---
+++ src/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Intro from './intro.jsx';
+
+const App = (props) => {
+  const setting = props.setting;
+
+  return <Intro gameTime={setting.GAME_TIME} allowableMistakesNumber={setting.ALLOWABLE_MISTAKES_NUMBER}/>;
+};
+
+export default App;
