0a2f12cc5e51b8a75d93eaff6530919c8b878efe
Akira <154.4k1r4@gmail.com>
2019-05-13 17:34:36 +0600

    NEW: Проведено snapshot тестирование

---
+++ src/components/intro-page/intro-page.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import IntroPage from './intro-page.jsx';
+
+it(`Отрисовка представления IntroPage`, () => {
+  const setting = {
+    GAME_TIME: 100,
+    ALLOWABLE_MISTAKES_NUMBER: 666
+  };
+
+  const view = renderer.create(
+      <IntroPage gameTime={setting.GAME_TIME} allowableMistakesNumber={setting.ALLOWABLE_MISTAKES_NUMBER}/>
+  ).toJSON();
+
+  expect(view).toMatchSnapshot();
+});
